7b4b5e71e1167f54473dd6adc4c241e7fbcf415e
Rashmika9712 <rashmikamadushansamarakoon@gmail.com>
2024-04-07 12:01:48 +0530

    Create Project

---
+++ 08_AuthSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _08_AuthSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
